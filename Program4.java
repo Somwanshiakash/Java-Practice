e635b4b56ea74ac81a99e022885ad1f8ae3d5105
Akash Somwanshi <88986981+Somwanshiakash@users.noreply.github.com>
2023-05-17 04:54:47 -0700

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,20 @@
+class Program4{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=1;j--)
+			{
+			  System.out.print(j);	
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+54321
+54321
+54321
+54321
+54321
+*/
